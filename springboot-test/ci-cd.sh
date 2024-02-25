6cd33a9dc04948d6a2a666c13ae2d50ba574b6d4
Tanner Barrett <tannerbarrett@Tanners-MacBook-Pro.local>
2024-02-25 15:58:35 -0700

    Create helm charts and modify cluster for knative gc

--- springboot-test/ci-cd.sh
+++ springboot-test/ci-cd.sh
@@ -1,33 +1,51 @@
 #!/bin/sh
 
-get_next_version_and_tag() {
+get_latest_version() {
     GIT_URL="https://github.com/tbarrett082/knative-take-home"
-    local latest_tag=$(git ls-remote --tags $GIT_URL | awk -F '/' '{print $NF}' | grep 'BUILD' | sed 's/^BUILD-//' | sort -V | tail -n 1)
-    local next_version
+    LATEST_TAG=$(git ls-remote --tags $GIT_URL | awk -F '/' '{print $NF}' | grep 'build' | sed 's/^build-//' | sort -V | tail -n 1)
+    REGISTRY_URL="ghcr.io/tbarrett082/knative-take-home/spring-test"
+}
 
-    if [ -z "$latest_tag" ]; then
-        next_version="BUILD-1"
-    else
-        next_version="BUILD-$((latest_tag + 1))"
-    fi
-
-    git tag "$next_version"
-    git push --tags
-    echo "Setting Docker tag to $next_version"
-    DOCKER_TAG=$next_version
+tag_next_version() {
+  get_latest_version
+  local next_version
+  
+  if [ -z "$LATEST_TAG" ]; then
+      next_version="build-1"
+  else
+      next_version="build-$((LATEST_TAG + 1))"
+  fi
+  
+  git tag "$next_version"
+  git push --tags
+  echo "Setting Docker tag to $next_version"
+  DOCKER_TAG=$next_version
 }
 
 
 build_and_push() {
   mvn clean install
-  get_next_version_and_tag
-  REGISTRY_URL="ghcr.io/tbarrett082/knative-take-home"
-  local image_name="$REGISTRY_URL/spring-test:$DOCKER_TAG"
-  echo $image_name
+  tag_next_version
+  IMAGE="$REGISTRY_URL:$DOCKER_TAG"
+  echo $IMAGE
   
-  docker build -t $image_name .
-  docker push "$image_name"
+  docker build -t $IMAGE .
+  docker push "$IMAGE"
+}
+
+knative_deploy() {
+  if [[ -z "$IMAGE" ]]; then
+    get_latest_version
+    IMAGE="$REGISTRY_URL:build-$LATEST_TAG"
+  fi
+  export IMAGE=$IMAGE
+  envsubst < knative/service.yaml > /tmp/service.yaml
+  cat /tmp/service.yaml
+  kubectl apply -f /tmp/service.yaml
+#  unset IMAGE && rm /tmp/service.yaml
+  kn service list
 }
 
 
 build_and_push
+knative_deploy
--- springboot-test/src/main/java/functions/api/TestController.java
+++ springboot-test/src/main/java/functions/api/TestController.java
@@ -1,6 +1,8 @@
 package functions.api;
 
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -10,8 +12,10 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("greeting")
 public class TestController {
+  private static final Logger LOGGER = LoggerFactory.getLogger(TestController.class);
   @GetMapping(produces = "application/json")
   public ResponseEntity<String> hello() {
-    return new ResponseEntity<>("Hello, world", HttpStatus.OK);
+    LOGGER.info("Received a greeting.");
+    return new ResponseEntity<>("Hello, world. This is a traffic switch test. I'm testing the rollout period", HttpStatus.OK);
   }
 }
